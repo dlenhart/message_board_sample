a777b0b9b569ceb9d1e9b72222e15118cdbf39e0
dlenhart <dlenhart@gmail.com>
2021-05-14 06:26:45 -0400

    removed stale api key accidently commited

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,31 +17,4 @@ class HomeController extends Controller
         'messages' => $messages
       ]);
     }
-
-    public function validateAddress() {
-
-      $base = "https://maps.googleapis.com/maps/api/geocode/json?address=";
-
-      $apiKey = "AIzaSyCEHEZWTJNGWq-tWYhL24JueZwDV8Akxpg";
-
-      $address = "3715 Kirklynn Drive";
-
-      $formattedAddress = str_replace(" ", "+", $address);
-
-      $url = $base . $formattedAddress . "&key=" . $apiKey;
-
-      $response = Http::get($url);
-
-      echo $response['status'];
-
-      if($response['status'] == "OK"){
-        echo $response['results'][0]['geometry']['location']['lat'];
-      }
-
-      //echo $response['results'][0]['geometry']['location'];
-
-
-      //echo \config('services.google.key');
-
-    }
 }
